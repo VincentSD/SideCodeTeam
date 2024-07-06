8a954ff667627d11c2830781dfbc11c91d109895
Michael-cmd-sys <michaelawunikofi@gmail.com>
2024-07-06 19:51:03 +0000

    wrote a simple injection script to avoid repeating statements in html by storing the files in a objects and dynamically rendering to page

--- team-pages/michaels-project/js/script.js
+++ team-pages/michaels-project/js/script.js
@@ -1,1 +1,53 @@
-// Start
+const mainEl = document.querySelector("main");
+
+const benefitsContentJson = [
+    {
+        imgName: "money",
+        descPhrase: "competitive pay and overtime"
+    },
+    {
+        imgName: "medical",
+        descPhrase: "health and insurance options"
+    },
+    {
+        imgName: "piggy-bank",
+        descPhrase: "401K matching programs"
+    },
+    {
+        imgName: "sun",
+        descPhrase: "pto and paid holidays"
+    },
+    {
+        imgName: "trophy",
+        descPhrase: "monthly rewards and team building"
+    },
+    {
+        imgName: "mug",
+        descPhrase: "free snacks and beverages"
+    },
+    {
+        imgName: "ping-pong",
+        descPhrase: "fjing fjong [trust us you'll love it]"
+    },
+    {
+        imgName: "apple",
+        descPhrase: "multi screen workspace and macbook"
+    },
+];
+
+
+for(let content of benefitsContentJson){
+    const newSectionEl = document.createElement("section");
+    newSectionEl.className = "grid-b-item";
+
+    const imgEl = document.createElement("img");
+    imgEl.src = `./image/${content.imgName}.png`;
+
+    const spanEl = document.createElement("span");
+    spanEl.textContent = content.descPhrase;
+    
+    newSectionEl.appendChild(imgEl);
+    newSectionEl.appendChild(spanEl);
+    mainEl.append(newSectionEl);
+
+}
